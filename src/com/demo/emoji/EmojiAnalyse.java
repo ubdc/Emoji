1763826422d37db008d59e54a79caec28d1a96ca
huangwangqiang <huangwangqiang@kuaishou.com>
2020-12-09 22:17:17 +0800

    emoji analyse: print emoji contain nonsurrogate char

--- src/com/demo/emoji/EmojiAnalyse.java
+++ src/com/demo/emoji/EmojiAnalyse.java
@@ -2,10 +2,10 @@ package com.demo.emoji;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-/**
- * 
- */
 public class EmojiAnalyse {
 	public static void main(String[] args) {
 		readEmojis();
@@ -15,9 +15,40 @@ public class EmojiAnalyse {
 		try {
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(EmojiAnalyse.class.getResource("full_emoji_list.txt").openStream()));
 			String line;
+			List<String> singleCharEmoji = new ArrayList<>();
+			List<String> unkonwCharEmoji = new ArrayList<>();
 			
 			while ((line = bufferedReader.readLine()) != null) {
-				System.out.println(parseEmoji(line));
+				String emoji = parseEmoji(line);
+				
+				if (emoji.length() >= 2) {
+					for (int i = 0; i < emoji.length(); i++) {
+						char c = emoji.charAt(i);
+						
+						if (!Character.isHighSurrogate(c) && !Character.isLowSurrogate(c)) {
+							unkonwCharEmoji.add(emoji);
+						}
+					}					
+				} else {
+					singleCharEmoji.add(emoji);
+				}
+			}
+			
+			Collections.sort(singleCharEmoji);
+			Collections.sort(unkonwCharEmoji);
+
+			for (int i = 0; i < singleCharEmoji.size(); i++) {
+				System.out.print(singleCharEmoji.get(i));
+			}
+			
+			System.out.println("\n");
+			
+			for (String emoji : unkonwCharEmoji) {
+				System.out.print(emoji);
+				for (int i = 0; i < emoji.length(); i++) {
+					System.out.print(" \\u" + Integer.toHexString(emoji.charAt(i)));
+				}
+				System.out.println();
 			}
 		} catch (Throwable e) {
 			e.printStackTrace();
